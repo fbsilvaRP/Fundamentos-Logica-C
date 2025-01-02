c5966525120b4b0d0fd7a08c83d0b250e7974bff
Felipe <168025584+fbsilvaRP@users.noreply.github.com>
2025-01-02 14:55:36 -0300

    Add files via upload

---
+++ Fatorial.c
@@ -0,0 +1,22 @@
+/*Algoritmo em C para calcular o fatorial de um número n digitado. Feito em 02/01/2024*/
+
+#include <stdio.h>
+int main()
+{	
+	int op = 1; 
+	while(op == 1)
+	{
+		int n, nfat = 1; 						//A variável nfat recebe n!	
+		printf("\n* * * Calculo fatorial * * * ");
+		printf("\nDigite o valor de n: ");
+		scanf("%d", &n);						//Recebe o valor de n
+		for(int i = 2; i <= n; i++)				//Cálculo de n!
+			nfat *= i;			
+		printf("* * * * * * * * * * * * * * * ");
+		printf("\n%d! = %d", n, nfat);			//Apresenta o resultado de n!
+		printf("\n* * * * * * * * * * * * * * * ");
+		printf("\nDeseja sair? Não(1) \nR: ");
+		scanf("%d", &op); 
+	}
+	return 0; 
+}
